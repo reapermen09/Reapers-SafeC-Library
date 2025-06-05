4e90b98fef8cf5316eb9c579ba1ef1e7bdde95d3
Reaper <87583659+reapermen09@users.noreply.github.com>
2025-06-04 20:38:09 -0400

    Update safec.h

--- src/safec.h
+++ src/safec.h
@@ -12,13 +12,22 @@
 
 #include <stddef.h>
 
-char* s_input(void);
+#ifdef SAFEC_DEBUG
+#include <stdio.h>
+#define s_log(fmt, ...) fprintf(stderr, "[SafeC] " fmt "\n", ##__VA_ARGS__)
+#else
+#define s_log(fmt, ...)
+#endif
 
-void s_print(const char* fmt, ...);
-
-void s_print_ln(const char* fmt, ...);
+extern void* (*s_malloc)(size_t);
+extern void* (*s_realloc)(void*, size_t);
+extern void  (*s_free)(void*);
 
 char* s_strdup(const char* str);
+
+char** s_strsplit(const char* str, char delim, size_t* out_count);
+
+void s_strsplit_free(char** arr, size_t count);
 
 char* s_strcat(char* dest, size_t* dest_size, const char* src);
 
@@ -30,8 +39,14 @@ char* s_substr(const char* str, size_t start, size_t length);
 
 char* s_strtrim(const char* str);
 
-char** s_strsplit(const char* str, char delim, size_t* out_count);
+char* s_file_read(const char* filename);
 
-void s_strsplit_free(char** arr, size_t count);
+int s_file_write(const char* filename, const char* data);
+
+char* s_input(void);
+
+void s_print(const char* fmt, ...);
+
+void s_print_ln(const char* fmt, ...);
 
 #endif
